7e5603272188b3eaa6770146eaad6af2350a0d7d
Daniil Yaskevich <eqxbarboss@gmail.com>
2020-09-04 00:52:56 +0300

    First parallel implementation

--- ComputerGraphicsAlgorithms/Renderer.cpp
+++ ComputerGraphicsAlgorithms/Renderer.cpp
@@ -1,5 +1,7 @@
 #include "Renderer.h"
 
+#include <thread>
+
 #include "Math.h"
 
 namespace cga
@@ -9,7 +11,9 @@ Renderer::Renderer(int aWidth, int aHeight, HDC aDeviceContext, std::function<vo
 	: width(aWidth),
 	height(aHeight),
 	deviceContext(aDeviceContext),
-	aInvalidateCallback(aInvalidateCallback)
+	aInvalidateCallback(aInvalidateCallback),
+	threadCount(std::thread::hardware_concurrency()),
+	threadPool(std::thread::hardware_concurrency())
 {
 
 }
@@ -43,39 +47,72 @@ void Renderer::Render(std::unique_ptr<Scene> &scene)
 	Obj obj = scene->obj;
 	Camera &camera = scene->camera;
 
-	auto model = glm::mat4(1.0f);
+	const auto model = glm::mat4(1.0f);
 	const auto view = camera.GetViewMatrix();
 	const auto projection = GetPerspectiveProjectionMatrix(width, height, 0.1f, 1000.0f, camera.FOV);
 	const auto viewPort = GetViewPortMatrix(width, height);
 
 	const auto pvm = projection * view * model;
 
-	for (int i = 0; i < obj.vertices.size(); i++)
+	int step = obj.vertices.size() / threadCount;
+	if (step == 0) step = obj.vertices.size();
+	int first = 0;
+
+	while (1)
 	{
-		obj.vertices[i] = pvm * obj.vertices[i];
+		int last = first + step;
+		if (last > obj.vertices.size()) last = obj.vertices.size();
 
-		obj.vertices[i].x = obj.vertices[i].x / obj.vertices[i].w;
-		obj.vertices[i].y = obj.vertices[i].y / obj.vertices[i].w;
-		obj.vertices[i].z = obj.vertices[i].z / obj.vertices[i].w;
-		obj.vertices[i].w = 1.0f;
+		threadPool.push([this, &obj, first, last, pvm, viewPort](int id)
+			{
+				this->CalculateVertices(obj.vertices, first, last, pvm, viewPort);
+			});
 
-		obj.vertices[i] = viewPort * obj.vertices[i];
+		if (last == obj.vertices.size()) break;
+		first += step;
 	}
+
+	while (1)
+	{
+		if (threadPool.n_idle() == threadCount) break;
+	}
+
+	//CalculateVertices(obj.vertices, 0, obj.vertices.size(), pvm, viewPort);
 
 	SelectObject(deviceContext, GetStockObject(WHITE_BRUSH));
 	Rectangle(deviceContext, 0, 0, width, height);
 
-	for (const auto& polygon : obj.polygons)
-	{
-		for (int i = 0; i < polygon.verticesIndices.size() - 1; i++)
-		{
-			RasterizeLine(deviceContext, obj.vertices[polygon.verticesIndices[i] - 1], obj.vertices[polygon.verticesIndices[i + 1] - 1]);
-		}
-
-		RasterizeLine(deviceContext, obj.vertices[polygon.verticesIndices[polygon.verticesIndices.size() - 1] - 1], obj.vertices[polygon.verticesIndices[0] - 1]);
-	}
+	DrawPolygons(obj.polygons, obj.vertices, 0, obj.polygons.size());
 
 	aInvalidateCallback();
+}
+
+void Renderer::CalculateVertices(std::vector<glm::vec4> &vertices, int first, int last, glm::mat4 pvm, glm::mat4 viewPort)
+{
+	for (int i = first; i < last; i++)
+	{
+		vertices[i] = pvm * vertices[i];
+
+		vertices[i].x = vertices[i].x / vertices[i].w;
+		vertices[i].y = vertices[i].y / vertices[i].w;
+		vertices[i].z = vertices[i].z / vertices[i].w;
+		vertices[i].w = 1.0f;
+
+		vertices[i] = viewPort * vertices[i];
+	}
+}
+
+void Renderer::DrawPolygons(std::vector<Polygon> &polygons, std::vector<glm::vec4> &vertices, int first, int last)
+{
+	for (int j = first; j < last; j++)
+	{
+		for (int i = 0; i < polygons[j].verticesIndices.size() - 1; i++)
+		{
+			RasterizeLine(deviceContext, vertices[polygons[j].verticesIndices[i] - 1], vertices[polygons[j].verticesIndices[i + 1] - 1]);
+		}
+
+		RasterizeLine(deviceContext, vertices[polygons[j].verticesIndices[polygons[j].verticesIndices.size() - 1] - 1], vertices[polygons[j].verticesIndices[0] - 1]);
+	}
 }
 
 }
--- ComputerGraphicsAlgorithms/Renderer.h
+++ ComputerGraphicsAlgorithms/Renderer.h
@@ -2,10 +2,15 @@
 
 #include <memory>
 #include <functional>
+#include <mutex>
+
+#include <ctpl/ctpl_stl.h>
+#include <glm/glm.hpp>
 
 #include "framework.h"
 
 #include "Scene.h"
+#include "Obj.h"
 
 namespace cga
 {
@@ -20,8 +25,15 @@ public:
 	void Render(std::unique_ptr<Scene> &scene);
 
 private:
+	ctpl::thread_pool threadPool;
+	std::mutex mutex;
+	int threadCount;
+
 	HDC deviceContext;
 	std::function<void()> aInvalidateCallback;
+
+	void CalculateVertices(std::vector<glm::vec4> &vertices, int first, int last, glm::mat4 pvm, glm::mat4 viewPort);
+	void DrawPolygons(std::vector<Polygon> &polygons, std::vector<glm::vec4> &vertices, int first, int last);
 };
 
 }
